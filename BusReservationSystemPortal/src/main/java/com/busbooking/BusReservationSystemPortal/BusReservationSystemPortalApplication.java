6e8756ade93b403bb4dce427fe3704834f10de87
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-10 10:23:02 +0530

    starting new project

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/BusReservationSystemPortalApplication.java
@@ -0,0 +1,13 @@
+package com.busbooking.BusReservationSystemPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusReservationSystemPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusReservationSystemPortalApplication.class, args);
+	}
+
+}
