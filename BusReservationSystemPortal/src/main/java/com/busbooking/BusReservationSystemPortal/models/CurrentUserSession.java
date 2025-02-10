d30b1d037f59df16976013c460ae29caab7aea1a
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-10 13:24:52 +0530

    adding module currentUserSession

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/CurrentUserSession.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/CurrentUserSession.java
@@ -3,22 +3,23 @@ package com.busbooking.BusReservationSystemPortal.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
 @Entity
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
-public class CurrentAdminSession {
+public class CurrentUserSession {
     @Id
     @Column(unique = true)
-    private Integer adminId;
+    private Integer userId;
 
     private String uuid;
 
     private LocalDateTime dateTime;
+
 }
