20ec2ec75f8bd3c1c69f9b3f7dbdecf81b939ab2
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-17 23:21:50 +0530

    adding UserService

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/UserService.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/UserService.java
@@ -1,12 +1,21 @@
 package com.busbooking.BusReservationSystemPortal.service;
 
+import com.busbooking.BusReservationSystemPortal.exception.AdminException;
 import com.busbooking.BusReservationSystemPortal.exception.UserException;
 import com.busbooking.BusReservationSystemPortal.models.User;
+
+import java.util.List;
 
 public interface UserService {
 
     public User createUser(User user)throws UserException;
 
     public  User updateUser(User user, String key)throws UserException;
+
+    public User deleteUser(Integer userId,String key) throws UserException, AdminException;
+
+    public User viewUserById(Integer userId, String key) throws UserException, AdminException;
+
+    public List<User> viewUsers(String key) throws UserException, AdminException;
 
 }
