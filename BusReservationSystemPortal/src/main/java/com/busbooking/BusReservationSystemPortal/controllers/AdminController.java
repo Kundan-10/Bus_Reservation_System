8f621cb26f94dbdccfa2cabd5dfe89837b2b5b13
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-18 22:10:44 +0530

    adding AdminController

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/controllers/AdminController.java
@@ -0,0 +1,35 @@
+package com.busbooking.BusReservationSystemPortal.controllers;
+
+import com.busbooking.BusReservationSystemPortal.exception.AdminException;
+import com.busbooking.BusReservationSystemPortal.models.Admin;
+import com.busbooking.BusReservationSystemPortal.models.User;
+import com.busbooking.BusReservationSystemPortal.service.AdminService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@Tag(name = "Admin Management", description = "Endpoints for Admin createAdmin and updateAdmin")
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @PostMapping("/admin/createAdmin")
+    @Operation(summary = "Add Admin", description = "Save a new Admin in the system")
+    ResponseEntity<Admin> createAdmin(@RequestBody Admin admin) throws AdminException {
+        Admin createadmin = adminService.createAdmin(admin);
+        return new ResponseEntity<Admin>(createadmin, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/admin/updateAdmin")
+    @Operation(summary = "Update Admin", description = "Update an existing Admin details")
+    ResponseEntity<Admin> updateAdmin(@RequestBody Admin admin, @RequestParam String key) throws AdminException {
+        Admin admin1 = adminService.updateAdmin(admin, key);
+        return new ResponseEntity<Admin>(admin1, HttpStatus.OK);
+    }
+
+}
