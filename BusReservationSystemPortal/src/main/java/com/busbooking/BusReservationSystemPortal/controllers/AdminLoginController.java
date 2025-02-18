c6d228869b0b5be3c345198336c7abb87799cdd1
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-18 22:11:08 +0530

    adding AdminLoginController

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/controllers/AdminLoginController.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/controllers/AdminLoginController.java
@@ -19,14 +19,14 @@ public class AdminLoginController {
 
     private final AdminLoginService adminLoginService;
 
-    @PostMapping("/login/admin")
+    @PostMapping("/admin/login")
     @Operation(summary = "Admin Login", description = "Authenticates an admin and returns a session")
     public ResponseEntity<CurrentAdminSession> logInAdmin(@Valid @RequestBody AdminLoginDTO adminLoginDTO) throws LoginException {
         CurrentAdminSession currentAdminSession = adminLoginService.logIntoAdminAccount(adminLoginDTO);
         return new ResponseEntity<CurrentAdminSession>(currentAdminSession, HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/logout/admin")
+    @PostMapping("/admin/logout")
     @Operation(summary = "Admin Logout", description = "Logs out an admin using a session key")
     public String  logOutAdmin(@RequestParam String key)throws LoginException{
         return adminLoginService.logOutFromAdminAccount(key);
