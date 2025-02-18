3bd807b73f3b454a0bcfdf3a0f2101e1a68ba7cd
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-18 22:13:06 +0530

    adding BusService

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/BusService.java
@@ -0,0 +1,24 @@
+package com.busbooking.BusReservationSystemPortal.service;
+
+import com.busbooking.BusReservationSystemPortal.exception.AdminException;
+import com.busbooking.BusReservationSystemPortal.exception.BusException;
+import com.busbooking.BusReservationSystemPortal.models.Bus;
+
+import java.util.List;
+
+public interface BusService {
+
+
+        public Bus addBus(Bus bus,String key) throws BusException, AdminException;
+
+        public Bus updateBus(Bus bus,String key) throws BusException, AdminException;
+
+        public Bus deleteBus(Integer busId,String key) throws BusException, AdminException;
+
+        public Bus viewBus(Integer busId) throws BusException;
+
+        public List<Bus> viewBusByType(String BusType) throws BusException;
+
+        public List<Bus> viewAllBuses() throws BusException;
+
+}
