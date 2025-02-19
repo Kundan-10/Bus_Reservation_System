d4f78d17abd858e4d5d328cb21a7431b3953349a
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-19 18:43:15 +0530

    adding RouteException

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/exception/RouteException.java
@@ -0,0 +1,7 @@
+package com.busbooking.BusReservationSystemPortal.exception;
+
+public class RouteException extends Exception{
+    public RouteException(String message) {
+        super(message);
+    }
+}
