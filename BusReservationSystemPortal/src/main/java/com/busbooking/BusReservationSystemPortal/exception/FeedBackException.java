ec35371727d8067b4df535db96b3d751d8745746
Kundan-10 <kundanwakudkar10@gmail.com>
2025-02-25 08:55:42 +0530

    adding FeedBackException

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/exception/FeedBackException.java
@@ -0,0 +1,8 @@
+package com.busbooking.BusReservationSystemPortal.exception;
+
+public class FeedBackException extends Exception{
+
+    FeedBackException(String smg){
+        super(smg);
+    }
+}
