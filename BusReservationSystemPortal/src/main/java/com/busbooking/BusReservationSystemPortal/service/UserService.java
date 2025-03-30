8d31fb2a8ab2baa6bb591364a8668455c9c4c988
Kundan-10 <kundanwakudkar10@gmail.com>
2025-03-30 07:03:20 +0530

    update UserService

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/UserService.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/UserService.java
@@ -1,21 +1,23 @@
 package com.busbooking.BusReservationSystemPortal.service;
 
+import com.busbooking.BusReservationSystemPortal.DTO.UserDTO;
 import com.busbooking.BusReservationSystemPortal.exception.AdminException;
 import com.busbooking.BusReservationSystemPortal.exception.UserException;
 import com.busbooking.BusReservationSystemPortal.models.User;
+import jakarta.validation.Valid;
 
 import java.util.List;
 
 public interface UserService {
 
-    public User createUser(User user)throws UserException;
+    public User createUser(@Valid UserDTO user)throws UserException;
 
-    public  User updateUser(User user, String key)throws UserException;
+    public  User updateUser(@Valid UserDTO user)throws UserException;
 
-    public User deleteUser(Integer userId,String key) throws UserException, AdminException;
+    public User deleteUser(Integer userId) throws UserException, AdminException;
 
-    public User viewUserById(Integer userId, String key) throws UserException, AdminException;
+    public User viewUserById(Integer userId) throws UserException, AdminException;
 
-    public List<User> viewUsers(String key) throws UserException, AdminException;
+    public List<User> viewUsers() throws UserException, AdminException;
 
 }
