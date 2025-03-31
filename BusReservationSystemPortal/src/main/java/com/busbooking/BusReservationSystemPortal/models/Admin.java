c38186d5f0e502cba64804d592e4c0ac8810ead4
Kundan-10 <kundanwakudkar10@gmail.com>
2025-03-31 17:25:56 +0530

    update module Admin

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/Admin.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/Admin.java
@@ -1,34 +1,38 @@
 package com.busbooking.BusReservationSystemPortal.models;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.busbooking.BusReservationSystemPortal.Enum.Role;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class Admin {
+public class Admin implements UserDetails {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer adminId;
 
     @NotNull(message = "Name cannot be null!")
-    @NotBlank(message = "Name connot be blank!")
+    @NotBlank(message = "Name connote be blank!")
     private String firstName;
 
     private String lastName;
 
     @NotNull(message="Password cannot be null!")
     @NotBlank(message= "Password cannot be blank!")
-    @Pattern(regexp = "[A-Za-z0-9!@#$%^&*_]{8,15}", message = "Password must be 8-15 characters in length and can include alphanumerics and special characters")
-    private String adminPassword;
+//    @Pattern(regexp = "[A-Za-z0-9!@#$%^&*_]{8,15}", message = "Password must be 8-15 characters in length and can include alphanumerics and special characters")
+    private String password;
 
     @NotNull(message="Mobile number cannot be null!")
     @NotBlank(message= "Mobile number cannot be blank!")
@@ -39,4 +43,17 @@ public class Admin {
     @Email
     private String email;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_"+ role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
