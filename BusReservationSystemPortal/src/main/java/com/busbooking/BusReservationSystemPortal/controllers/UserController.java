ffe903f9672b9d63a34c7fd1e201a48719af5f29
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-02 00:55:31 +0530

    update UserController

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/controllers/UserController.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.busbooking.BusReservationSystemPortal.controllers;
 
+import com.busbooking.BusReservationSystemPortal.DTO.UserDTO;
 import com.busbooking.BusReservationSystemPortal.exception.AdminException;
 import com.busbooking.BusReservationSystemPortal.exception.UserException;
 import com.busbooking.BusReservationSystemPortal.models.User;
@@ -24,36 +25,36 @@ public class UserController {
 
     @PostMapping("/users")
     @Operation(summary = "Add User", description = "Save a new user in the system")
-    public ResponseEntity<User> saveUser(@Valid @RequestBody User user) throws UserException {
+    public ResponseEntity<User> saveUser(@Valid @RequestBody UserDTO user) throws UserException {
         User saveuser = userService.createUser(user);
         return new ResponseEntity<User>(saveuser, HttpStatus.CREATED);
     }
 
-    @PutMapping("/users")
+    @PutMapping("/updateUser")
     @Operation(summary = "Update User", description = "Update an existing user's details")
-    public ResponseEntity<User> updateUser(@Valid @RequestBody User user, @RequestParam(required = false) String key) throws UserException {
-        User udpateuser = userService.updateUser(user, key);
+    public ResponseEntity<User> updateUser(@Valid @RequestBody UserDTO user) throws UserException {
+        User udpateuser = userService.updateUser(user);
         return new ResponseEntity<>(udpateuser, HttpStatus.OK);
     }
 
     @DeleteMapping("/admin/{userId}")
     @Operation(summary = "Delete User", description = "Delete a user by ID (Admin access required)")
-    public ResponseEntity<User> deleteUser(@PathVariable("userId") Integer userId, @RequestParam(required = false) String key) throws UserException, AdminException {
-        User deleteuser = userService.deleteUser(userId, key);
+    public ResponseEntity<User> deleteUser(@PathVariable("userId") Integer userId) throws UserException, AdminException {
+        User deleteuser = userService.deleteUser(userId);
         return new ResponseEntity<User>(deleteuser, HttpStatus.OK);
     }
 
     @GetMapping("/admin/{userId}")
     @Operation(summary = "View User by ID", description = "View a user's details by ID (Admin access required)")
-    public ResponseEntity<User> viewUser(@PathVariable("userId") Integer userId, @RequestParam(required = false) String key) throws AdminException, UserException {
-        User viewuser = userService.viewUserById(userId, key);
+    public ResponseEntity<User> viewUser(@PathVariable("userId") Integer userId) throws AdminException, UserException {
+        User viewuser = userService.viewUserById(userId);
         return new ResponseEntity<User>(viewuser, HttpStatus.OK);
     }
 
     @GetMapping("/admin")
     @Operation(summary = "View All Users", description = "View a list of all users (Admin access required)")
-    public ResponseEntity<List<User>> viewUsers(@RequestParam(required = false) String key) throws AdminException, UserException {
-        List<User> users = userService.viewUsers(key);
+    public ResponseEntity<List<User>> viewUsers() throws AdminException, UserException {
+        List<User> users = userService.viewUsers();
         return new ResponseEntity<List<User>>(users, HttpStatus.OK);
     }
 
