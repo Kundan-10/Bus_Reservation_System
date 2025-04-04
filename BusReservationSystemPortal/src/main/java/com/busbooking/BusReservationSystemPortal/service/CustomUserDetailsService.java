bbd85fffd81d1d23e42c63fc8a5e5d2741e8aaee
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-04 18:51:25 +0530

    adding CustomUserDetailsService

---
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.busbooking.BusReservationSystemPortal.service;
+
+import com.busbooking.BusReservationSystemPortal.models.User;
+import com.busbooking.BusReservationSystemPortal.repositoty.UserDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserDao userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+        return new CustomUserDetails(user);
+    }
+}
