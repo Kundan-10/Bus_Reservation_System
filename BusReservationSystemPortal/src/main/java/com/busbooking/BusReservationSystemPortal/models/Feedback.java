3ed7dde5b7dbb5928affd700ec865adfa8622e47
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-04 18:51:49 +0530

    update Feedback

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/Feedback.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/models/Feedback.java
@@ -16,7 +16,7 @@ import java.time.LocalDateTime;
 public class Feedback {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Integer feedBackId;
+    private Integer feedbackId;
 
     private Integer driverRating;
 
