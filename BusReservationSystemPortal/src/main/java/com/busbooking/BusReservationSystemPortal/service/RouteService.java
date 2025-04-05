d100fa007bc89b958614b7d621b2bf74f2b7507d
Kundan-10 <kundanwakudkar10@gmail.com>
2025-04-06 00:51:34 +0530

    update RouteService

--- BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/RouteService.java
+++ BusReservationSystemPortal/src/main/java/com/busbooking/BusReservationSystemPortal/service/RouteService.java
@@ -2,17 +2,18 @@ package com.busbooking.BusReservationSystemPortal.service;
 
 import com.busbooking.BusReservationSystemPortal.exception.AdminException;
 import com.busbooking.BusReservationSystemPortal.exception.RouteException;
+import com.busbooking.BusReservationSystemPortal.exception.UserException;
 import com.busbooking.BusReservationSystemPortal.models.Route;
 
 import java.util.List;
 
 public interface RouteService {
 
-    public Route addRoute(Route route, String key) throws RouteException, AdminException;
+    public Route addRoute(Route route) throws RouteException, AdminException, UserException;
 
-    public Route updateRoute(Route route,String key) throws RouteException, AdminException;
+    public Route updateRoute(Route route) throws RouteException, AdminException, UserException;
 
-    public Route deleteRoute(int routeId,String key) throws RouteException, AdminException;
+    public Route deleteRoute(int routeId) throws RouteException, AdminException, UserException;
 
     public Route viewRoute(int routeId) throws RouteException;
 
